4a73cca5f8f2b58d1f1d7505a6a1d69c5fb03834
Juan Pablo Tobar <juanpablococtm@gmail.com>
2024-11-11 19:36:49 -0500

    Mejorar la interfaz de usuario y la funcionalidad de la aplicación Todo: actualizar estilos, manejar el estado de edición y limpiar el formulario después de agregar una tarea.

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -4,36 +4,51 @@ export const Todo = ({ item, onUpdate, onDelete }) => {
   const [isEdit, setIsEdit] = useState(false);
 
   const FormEdit = () => {
-
     const [newValue, setNewValue] = useState(item.title);
     const handleSubmit = (e) => {
       e.preventDefault();
       setIsEdit(false);
-    }
+    };
 
-    
     const handleChange = (e) => {
       const value = e.target.value;
       setNewValue(value);
-    }
+    };
 
-    const handleClick = () => {
+    const handleClickUpdateTodo = () => {
+      onUpdate(item.id, newValue);
       setIsEdit(false);
-    }
+    };
 
     return (
-      <form onSubmit={handleSubmit} >
+      <form onSubmit={handleSubmit} className="text-white">
         <input onChange={handleChange} value={newValue} type="text" />
-        <button onClick={handleClick} >Update</button>
+        <button onClick={handleClickUpdateTodo}>Update</button>
       </form>
     );
   };
 
   const TodoElement = () => {
     return (
-      <div>
-        {item.title} <button onClick={() => setIsEdit(true)}>Edit</button>
-        <button onClick={ () => {handleDelete} } >Delete</button>
+      <div className="bg-[#3e3c3d] mb-2 px-20 py-5 rounded-lg max-w-80 flex flex-col items-center justify-center ">
+        <div className="text-xl font-bold" >{item.title}</div>
+
+        <div>
+          <button
+            className="bg-[#ff8000] px-3 py-2 ml-2 rounded-lg"
+            onClick={() => setIsEdit(true)}
+          >
+            Edit
+          </button>
+          <button
+            className="bg-[#c92c3e] px-3 py-2 ml-2 rounded-lg"
+            onClick={() => {
+              onDelete(item.id);
+            }}
+          >
+            Delete
+          </button>
+        </div>
       </div>
     );
   };
--- src/components/TodoApp.jsx
+++ src/components/TodoApp.jsx
@@ -2,9 +2,9 @@ import { useState } from "react"; // Importamos useState para manejar el estado 
 import { Todo } from "./Todo";
 
 export const TodoApp = () => {
-  // Inicialización del estado con un valor inicial "hola"
+  // Inicialización del estado con un valor inicial de ""
   // title: contiene el valor actual del estado | setTitle: función para actualizar el estado
-  const [title, setTitle] = useState("hola");
+  const [title, setTitle] = useState("Hola");
   const [todos, setTodos] = useState([]);
 
   // onChange es la función que actualiza el valor de title cada vez que se escribe en el input
@@ -14,40 +14,57 @@ export const TodoApp = () => {
   };
 
   const handleSubmit = (e) => {
-    e.preventDefault()
+    e.preventDefault();
     const newTodo = {
       id: crypto.randomUUID(),
       title: title,
       completed: false,
     };
     setTodos([...todos, newTodo]);
+    setTitle("");
   };
 
   const handleUpdate = (id, value) => {
     const temp = [...todos];
-    const item = temp.find((item) => item.id === id)
-    item.title = value
-    setTodos(temp)
-  }
-  
+    const item = temp.find((item) => item.id === id);
+    item.title = value;
+    setTodos(temp);
+  };
+
   const handleDelete = (id) => {
     const temp = todos.filter((item) => item.id !== id);
     setTodos(temp);
-
-  }
+  };
 
   return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        {/* Input muestra y controla el valor de title */}
-        <input onChange={onChange} type="text" value={title} />
-        <button onClick={handleSubmit} type="submit">
+    <div className="flex flex-col items-center justify-center mt-60" >
+      <h1 className="text-white text-3xl mb-4" >Todo App</h1>
+      <form
+        onSubmit={handleSubmit}
+        className=""
+      >
+        <input
+          onChange={onChange}
+          type="text"
+          value={title}
+          className="mb-4 p-2 w-96 border rounded-md"
+        />
+        <button
+          onClick={handleSubmit}
+          type="submit"
+          className=" ml-2 px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 "
+        >
           Add
         </button>
-      </form>
-      <div>
+      </form >
+      <div className="text-white flex flex-col items-center justify-center" >
         {todos.map((item) => (
-          <Todo key={item.id} item = {item} onUpdate={handleUpdate} onDelete={handleDelete} />
+          <Todo
+            key={item.id}
+            item={item}
+            onUpdate={handleUpdate}
+            onDelete={handleDelete}
+          />
         ))}
       </div>
     </div>
